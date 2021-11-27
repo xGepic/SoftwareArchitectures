2cef6e6acf56a61c3b85912f98259cd0be349f71
Steve <simanek.s@gmx.at>
2021-11-27 01:57:59 +0100

    PasswordSafe initial commit

---
+++ PasswordSafe/CipherFacility.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordSafeConsole
+{
+    internal class CipherFacility
+    {
+        private string masterPw;
+
+        public CipherFacility(string masterPw)
+        {
+            this.masterPw = masterPw;
+        }
+
+        public string Decrypt(byte[] crypted)
+        {
+            var key = GetKey(this.masterPw);
+
+            using (var aes = Aes.Create())
+            using (var decryptor = aes.CreateDecryptor(key, key))
+            {
+                var decryptedBytes = decryptor
+                    .TransformFinalBlock(crypted, 0, crypted.Length);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+
+        public byte[] Encrypt(string plain)
+        {
+            var key = GetKey(this.masterPw);
+            using (var aes = Aes.Create())
+            using (var encryptor = aes.CreateEncryptor(key, key))
+            {
+                var plainText = Encoding.UTF8.GetBytes(plain);
+                return encryptor.TransformFinalBlock(plainText, 0, plainText.Length);
+            }
+        }
+
+        private static byte[] GetKey(string password)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(password);
+            using (var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(keyBytes);
+            }
+        }
+    }
+}
---
+++ PasswordSafe/MasterPasswordRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace PasswordSafeConsole
+{
+    internal class MasterPasswordRepository
+    {
+        private string masterPasswordPath;
+
+        public MasterPasswordRepository(string masterPasswordPath)
+        {
+            this.masterPasswordPath = masterPasswordPath;
+        }
+
+        internal bool MasterPasswordIsEqualTo(string masterPwToCompare)
+        {
+            return File.Exists(this.masterPasswordPath) &&
+                masterPwToCompare == File.ReadAllText(this.masterPasswordPath);
+        }
+
+        internal void SetMasterPasswordPlain(string masterPw)
+        {
+            File.WriteAllText(this.masterPasswordPath, masterPw);
+        }
+    }
+}
